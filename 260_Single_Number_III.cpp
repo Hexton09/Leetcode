3de8a672c56b8f2d42e246b2f95974d4a1cfc4e3
Rahul Singh nagesh <98824774+Hexton09@users.noreply.github.com>
2023-10-05 00:35:05 +0530

    Update 260_Single_Number_III.cpp

--- 260_Single_Number_III.cpp
+++ 260_Single_Number_III.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-       vector<int> ans;
        sort(nums.begin(),nums.end());
        for(int i=0;i<nums.size();i++){
            if(i+1<nums.size() && nums[i]==nums[i+1]){
