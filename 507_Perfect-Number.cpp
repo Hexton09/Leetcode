411979ad569936138f1d17112351d607dcb4e5a6
Rahul Singh nagesh <98824774+Hexton09@users.noreply.github.com>
2023-10-11 00:59:17 +0530

    Create 507_Perfect-Number.cpp

---
+++ 507_Perfect-Number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int count=1;
+        if(num==1)
+        {
+            return false;
+        }
+        for(int i=2;i<=sqrt(num);i++)
+        {
+            if(num %i==0)
+            {
+                count+=i+num/i;
+            }
+        }
+        return count==num;
+        
+    }
+};
