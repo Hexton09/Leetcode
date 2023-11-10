f328006c6b425160eb642a9670cf9fa88eaf9143
Rahul Singh Nagesh <98824774+Hexton09@users.noreply.github.com>
2023-11-10 23:23:32 +0530

    Create 1464_Maximum_Product_of_Two_Elements_in_an_Array.cpp

---
+++ 1464_Maximum_Product_of_Two_Elements_in_an_Array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        int ans= (nums[n-1]-1)*(nums[n-2]-1);
+        return ans;
+        
+    }
+};
