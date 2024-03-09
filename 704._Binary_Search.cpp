a37a0fc66efec34ea557284fbc0cdfb9cb71cd10
Rahul Singh Nagesh <98824774+Hexton09@users.noreply.github.com>
2024-03-09 20:50:55 +0530

    Create 704._Binary_Search.cpp

---
+++ 704._Binary_Search.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+                int e=nums.size()-1;
+        int s=0,mid;
+
+        while(s<=e)
+        {
+            mid=(s+e)/2;
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            else if(nums[mid]>target)
+            {
+                e=mid-1;
+            }
+            else
+            {
+                s=mid+1;
+            }
+        }
+
+       return -1; 
+        
+    }
+};
