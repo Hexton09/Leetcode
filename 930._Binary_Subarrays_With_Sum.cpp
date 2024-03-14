ad2ec787a7e508815b80dd344ee84cfdfd46db69
Rahul Singh Nagesh <98824774+Hexton09@users.noreply.github.com>
2024-03-14 23:20:08 +0530

    Create 930._Binary_Subarrays_With_Sum.cpp

---
+++ 930._Binary_Subarrays_With_Sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int count = 0;
+        int prefixSum = 0;
+        unordered_map<int, int> prefixSumCount;
+        
+        prefixSumCount[0] = 1; // Initialize prefix sum count with 0
+        
+        for (int num : nums) {
+            prefixSum += num;
+            count += prefixSumCount[prefixSum - goal];
+            prefixSumCount[prefixSum]++;
+        }
+        
+        return count;
+        
+    }
+};
