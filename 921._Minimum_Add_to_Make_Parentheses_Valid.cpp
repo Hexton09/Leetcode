c51da63e93370d896dd848d99dddf3963b980fd7
Rahul Singh Nagesh <98824774+Hexton09@users.noreply.github.com>
2024-03-29 01:40:43 +0530

    Create 921._Minimum_Add_to_Make_Parentheses_Valid.cpp

---
+++ 921._Minimum_Add_to_Make_Parentheses_Valid.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+
+    stack<char> st;
+
+    for (char ch : s) {
+        if (ch == '(')
+            st.push(ch);
+        else {
+            if (!st.empty() && st.top() == '(')
+                st.pop();
+            else
+                st.push(ch);
+        }
+    }
+
+    return st.size();
+        
+    }
+};
